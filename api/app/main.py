ecb553e35a0ec7b1f64a9bf38b65cc9e843e6812
hedon954 <171725713@qq.com>
2026-01-31 13:35:13 +0800

    feat: implement logging system and integrate it into main application

--- api/app/main.py
+++ api/app/main.py
@@ -1,15 +1,17 @@
+import logging
+from app.infrastructure.logging import setup_logging
 from core.config import get_settings
 
 from fastapi import FastAPI
 
 # 1. 加载配置信息
 settings = get_settings()
-print(settings)
 
+# 2. 初始化日志系统
+setup_logging()
+logger = logging.getLogger(__name__)
+logger.info("测试~")
+
+# 启动 fastapi
+# 运行方式：uvicorn app.main:app --reload --port 9527
 app = FastAPI()
-
-def main():
-    print("Hello from mini-manus!")
-
-if __name__ == "__main__":
-    main()
