37474c7f3a61b77d243d1497ec74a0854575f94a
hedon954 <171725713@qq.com>
2026-04-20 22:55:16 +0800

    fix(redis_stream_task): re-raise CancelledError to ensure proper task cancellation handling
    - Modify exception handling in RedisStreamTask to re-raise asyncio.CancelledError, allowing for better management of task cancellations and ensuring that the cancellation flow is correctly propagated.

--- api/app/infrastructure/external/task/redis_stream_task.py
+++ api/app/infrastructure/external/task/redis_stream_task.py
@@ -51,6 +51,7 @@ class RedisStreamTask(Task):
             await self._task_runner.invoke(self)
         except asyncio.CancelledError as e:
             logger.info(f"任务[{self._id}]执行被取消")
+            raise
         except Exception as e:
             logger.error(f"任务[{self.id}]执行出现异常: {str(e)}")
         finally:
